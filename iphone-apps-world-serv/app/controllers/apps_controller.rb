ba3133b30a91ac3ac1c2b332e81f33d2d36d7305
LittleCatBear <j.huguet9@gmail.com>
2015-02-07 12:49:10 +1300

    app controller

--- iphone-apps-world-serv/app/controllers/apps_controller.rb
+++ iphone-apps-world-serv/app/controllers/apps_controller.rb
@@ -1,28 +1,36 @@
 class AppsController < AdminsController
 
-  def new
-  end
+ # def new
+  #end
 
   def create
+    @cat = Category.find(params[:category_id])
+    @app = @cat.apps.create(app_params)
+    redirect_to @cat
   end
 
   def destroy
+    @cat = Category.find(params[:category_id])
+    @app = @cat.apps.find(params[:id])
+    @app.destroy
+    redirect_to category_path(@cat)
   end
 
   def edit
   end
 
   def update
+    @cat = Category.find(params[:category_id])
+    @app = @cat.apps.update(app_params)
   end
 
-  def show
-  end
+  #def show
+  #end
 
-  def index
-  end
-
-  def getAppsByCat
-
-  end
+ # def index
+ # end
   
-end
+  private 
+  def app_params
+    params.require(:app).permit(:url, :name, :price, :description)
+  end
