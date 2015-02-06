0207c2000fec53902447e50db3071f5eaf65caf3
LittleCatBear <j.huguet9@gmail.com>
2015-02-06 21:48:57 +1300

    hotfix

--- iphone-apps-world-serv/db/seeds.rb
+++ iphone-apps-world-serv/db/seeds.rb
@@ -23,7 +23,7 @@ apps = App.create([
  		analysis: "blablabla analysis twitter1",
  		pros: "blabla pros1",
  		cons: "blabla cons1",
- 		url: "https://itunes.apple.com/us/app/twitter/id333903271?mt=8"
+ 		url: "https://itunes.apple.com/us/app/twitter/id333903271?mt=8",
  		category: categories.first,
  		released: true
  	},
@@ -35,7 +35,7 @@ apps = App.create([
  		pros: "blabla pro youtube",
  		cons: "blabla con youtube",
  		released: true,
- 		url: "https://itunes.apple.com/us/app/youtube/id544007664?mt=8"
+ 		url: "https://itunes.apple.com/us/app/youtube/id544007664?mt=8",
  		category: categories.first
  	}
  ])
