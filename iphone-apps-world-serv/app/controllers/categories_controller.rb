a62c502defa33c89c1ef200952dcb5e714941a66
LittleCatBear <j.huguet9@gmail.com>
2015-02-09 22:27:28 +1300

    edit pros

--- iphone-apps-world-serv/app/controllers/categories_controller.rb
+++ iphone-apps-world-serv/app/controllers/categories_controller.rb
@@ -27,7 +27,7 @@ class CategoriesController < AdminsController
 
   def update
     @category = Category.find(params[:id])
-    if@category.update(category_params)
+    if @category.update(category_params)
       redirect_to categories_path
     else
       render 'edit'
--- iphone-apps-world-serv/app/controllers/pros_controller.rb
+++ iphone-apps-world-serv/app/controllers/pros_controller.rb
@@ -1,19 +1,37 @@
 class ProsController < AdminsController
-	def create
-		#@category = Category.find(params[:category_id])
+	def new
+		@pro = Pro.new
+  	end
+
+  	def create
+		
     	@app = App.find(params[:id])
     	@pro = @app.pros.create(pro_params)
-    	redirect_to @app
-  	end
-  	
-  	def new
+    	redirect_to app_path(@app)
   	end
 
   	def index 
   	end
 
+  	def destroy
+  		@app = App.find(params[:app_id])
+  		@pro = Pro.find(params[:id])
+  		@pro.destroy
+
+  		redirect_to app_path(@app)
+  	end
+
   	def edit
   		@pro = Pro.find(params[:id])
+  	end
+
+  	def update
+  		@pro = Pro.find(params[:id])
+   		if @pro.update(pro_params)
+   		   	redirect_to app_path(@pro.app_id)
+    	else
+      		render 'edit'
+    	end
   	end
 
   	def show
@@ -21,4 +39,9 @@ class ProsController < AdminsController
     	@app = App.find(params[:id])
     	@pro = @app.pros.build
   	end
+
+  	  private
+    def pro_params
+      params.require(:pro).permit(:content)
+    end
 end
