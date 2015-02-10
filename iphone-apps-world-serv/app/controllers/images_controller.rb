e07cda433c7c535c088704fa5d52041c2310913c
LittleCatBear <j.huguet9@gmail.com>
2015-02-10 17:10:13 +1300

    images

--- iphone-apps-world-serv/app/controllers/images_controller.rb
+++ iphone-apps-world-serv/app/controllers/images_controller.rb
@@ -1,23 +1,47 @@
 class ImagesController < AdminsController
-  
-  def new
+
+  def new 
+    @app = App.find(params[:app_id])
+    @image = @app.images.new
   end
 
   def create
-  end
-
-  def destroy
+    @app = app.find(params[:app_id])
+    @image = @app.images.create(image_params)
+    redirect_to app_path(@app)
   end
 
   def edit
+      @image = Image.find(params[:id])
+  end
+
+  def destroy
+    @app = App.find(params[:app_id])
+    @image = @app.images.find(params[:id])
+    @image.destroy
+    redirect_to app_path(@app)
   end
 
   def update
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+      redirect_to app_path(@image.app_id)
+    else
+      render 'edit'
+    end
   end
 
+
+#json utile pour le client
   def show
+    @app = App.find(params[:app_id])
+    #@image = @app.images.find(params[:id])
+   # render json: @image
   end
 
-  def index
-  end
+  private
+    def image_params
+      params.require(:image).permit(:name, :image)
+    end
+
 end
