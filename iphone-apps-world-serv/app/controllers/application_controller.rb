88c173cdf4d72c4774208f187bba1e5758646d43
LittleCatBear <j.huguet9@gmail.com>
2015-02-13 09:32:14 +1300

    clean code

--- iphone-apps-world-serv/app/controllers/application_controller.rb
+++ iphone-apps-world-serv/app/controllers/application_controller.rb
@@ -12,13 +12,15 @@ class ApplicationController < ActionController::Base
   end
 
 #a modifier une fois le token en place
+#la requête prend un objet d'apps avec le champ 
+#:images.id AS id_image aggrégé en +
   def get_articles_with_logo_image(apps)
     	
     	apps.each do |i|
-    		puts(get_image_url(i.id_image))
+    		puts(get_image_url(app.id_image))
     		@urls = {}
-    		@urls[:url] = get_image_url(i.id_image)
-    		@urls[:app] = i
+    		@urls[:url] = get_image_url(app.id_image)
+    		@urls[:app] = app
     		@macro.push(@urls)
     	end
     	return @macro
