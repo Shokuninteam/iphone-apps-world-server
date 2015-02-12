b4c8047cedc9c6e1483cca50356a27b9dec3eb34
LittleCatBear <j.huguet9@gmail.com>
2015-02-13 09:30:50 +1300

    clean code

--- iphone-apps-world-serv/app/controllers/application_controller.rb
+++ iphone-apps-world-serv/app/controllers/application_controller.rb
@@ -3,18 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-	def get_url
-		@url = get_image_url(params[:id])
-		return @url
-	end
-
 	 def get_image_url(id)
 	    @image = Image.find(id)
 	    @article = App.find(@image.app_id)
 	    @a = @image.image.url
 	    #@a.push(URI.join(request.url, @image.image.url), @article.id)
     return  @a 
+  end
+
+#a modifier une fois le token en place
+  def get_articles_with_logo_image(apps)
+    	
+    	apps.each do |i|
+    		puts(get_image_url(i.id_image))
+    		@urls = {}
+    		@urls[:url] = get_image_url(i.id_image)
+    		@urls[:app] = i
+    		@macro.push(@urls)
+    	end
+    	return @macro
   end
 
 
--- iphone-apps-world-serv/app/controllers/globals_controller.rb
+++ iphone-apps-world-serv/app/controllers/globals_controller.rb
@@ -2,35 +2,18 @@ class GlobalsController < ApplicationController
 
 	def index
 		@top10 = App.joins(:top10s).where(released: true).order("rank").order("top10s.updated_at DESC").group("rank")
-    	@articles = App.joins(:images).where(released: true).limit(10).order("apps.updated_at DESC").uniq
+    	#@articles = App.joins(:images).where(released: true).limit(10).order("apps.updated_at DESC").uniq
     	@categories = Category.all
     	@count = App.count(released: true)
     	#@urls = {}
-    	@macro=[]
-   		@result = App.joins(:images).select("images.id AS id_image, apps.*").where(released: true).limit(10).order("apps.updated_at DESC").uniq
-    	@categories = Category.all
-    	
-	    	@result.each do |i|
-	    		#@temp = get_image_url(i.id)
-	    		#i << get_image_url(i.id)
-	    		puts(get_image_url(i.id_image))
-	    		@urls = {}
-	    		@urls[:url] = get_image_url(i.id_image)
-	    		@urls[:app] = i
-	    		@macro.push(@urls)
-	    		
-	    		#@combined = i+@temp
-	    	end
-	    	
-	    	#@resultatsarticles = @articles.joins(@urls[2]).where(id: @urls[2])
-	    	#@resultats = @urls.joins(@result).select(@urls, @result).where(id_app: @result)
-	
-    	#@result = @articles << @urls
-		#@count = @articles.count()
+    	@result = App.joins(:images).select("images.id AS id_image, apps.*").where(released: true).limit(10).order("apps.updated_at DESC").uniq
+    	@macro = []
+   		@macro = get_articles_with_logo_image(@result)
+	    
 		render :json => {
-			#top10: @top10,
+			top10: @top10,
   			#articles: @articles,
-  			#categories: @categories,
+  			categories: @categories,
   			#images: @images,
   			#result: @result,
   			#combined: @combined,
