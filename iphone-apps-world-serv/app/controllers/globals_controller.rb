be8d0f5ee7adc013282c1aefa6e337a5fd5b91e7
LittleCatBear <j.huguet9@gmail.com>
2015-02-13 11:52:41 +1300

    image for TOP10

--- iphone-apps-world-serv/app/controllers/globals_controller.rb
+++ iphone-apps-world-serv/app/controllers/globals_controller.rb
@@ -1,25 +1,21 @@
 class GlobalsController < ApplicationController
 
 	def index
-		@top10 = App.joins(:top10s).where(released: true).order("rank").order("top10s.updated_at DESC").group("rank")
+		@top10 = App.joins(:top10s).joins(:images).select("images.id AS id_image, apps.*").where(released: true).order("rank").order("top10s.updated_at DESC").group("rank")
     	#@articles = App.joins(:images).where(released: true).limit(10).order("apps.updated_at DESC").uniq
     	@categories = Category.all
     	@count = App.count(released: true)
-
     	@result = App.joins(:images).select("images.id AS id_image, apps.*").where(released: true).limit(10).order("apps.updated_at DESC").uniq
-    	@macro = []
-   		@macro = get_articles_with_logo_image(@result)
+    	
+    	@macroArticles = []
+   		@macroArticles = get_articles_with_logo_image(@result)
 	    
+	    @macroTop10 = []
+	    @macroTop10 = get_articles_with_logo_image(@top10)
 		render :json => {
-			top10: @top10,
-  			#articles: @articles,
+			top10: @macroTop10,
   			categories: @categories,
-  			#images: @images,
-  			#result: @result,
-  			#combined: @combined,
-  			#resultatsarticles: @resultatsarticles,
-  			macro: @macro,
-  			#urls: @urls,
+  			macro: @macroArticles,
   			total: @count
 			 }
 	end
